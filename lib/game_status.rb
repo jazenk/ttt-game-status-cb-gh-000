07fe9821f0556b4001a61e49a9d68b7ca1875bbc
jake zenk <jake.zenk@ymail.com>
2018-04-18 20:50:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,10 +28,10 @@ def won?
   position_2 = board[win_index_2]
   position_3 = board[win_index_3]
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
-    else
+  else
       false
-    end
+  end
   end
 end
