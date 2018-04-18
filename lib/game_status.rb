7dab524198fa94e303b6182cd6b98271b88369bf
jake zenk <jake.zenk@ymail.com>
2018-04-18 21:11:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,21 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS [
-#horizontal
-[0,1,2],
-[3,4,5],
-[6,7,8],
-#vertical
-[0,3,6],
-[1,4,7],
-[2,5,8],
-#diagonal
-[0,4,8],
-[6,4,2]
-]
-
 def won?
   WIN_COMBINATIONS.each do |win_combination|
   win_index_1 = win_combination[0]
@@ -35,6 +20,20 @@ def won?
   end
   end
 end
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS [
+#horizontal
+[0,1,2],
+[3,4,5],
+[6,7,8],
+#vertical
+[0,3,6],
+[1,4,7],
+[2,5,8],
+#diagonal
+[0,4,8],
+[6,4,2]
+]
 
 def full? (board)
   full_board = true
